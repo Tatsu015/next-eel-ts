ef89f535fe573dfac90ddd49c184a71c0694846b
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-01-24 23:14:18 +0900

    dev and prod same become simply

--- next-eel-ts/main.py
+++ next-eel-ts/main.py
@@ -1,10 +1,26 @@
 import eel
 
 
-def main():
-    eel.init("../out")
-    eel.start("index.html", size=(1024, 768), port=8080)
-    print("aaaaaaaaaaaaaa")
+def start_eel(dev:bool):
+    if dev == True:
+        print('dev mode')
+        dir = '../out'
+        app = None
+        page = {'port':3000}
+    else:
+        print('prod mode')
+        dir = 'out'
+        app = 'chrome'
+        page='index.html'
+
+    eel.init(dir)
+    eel.start(page, mode=app, host='localhost', size=(1024, 768), port=8080)
 
 if __name__ == "__main__":
-    main()
+    import sys
+    try:
+        dev = (sys.argv[1] == "dev")
+    except:
+        dev = False
+
+    start_eel(dev)
